915b06abbf924e13c46b3fe950deeb1ff6cc76fb
nshine787 <sovietconscipt787@gmail.com>
2020-11-07 00:12:27 -0800

    combine left and right cols

--- functions/functions-project-measure.R
+++ functions/functions-project-measure.R
@@ -1,47 +1,61 @@
-# prepareMeasureData(measure)
+# prepareMeasureData(measure.df)
+# takes the file location passed in from 'measure' and then returns a cleaned dataframe
 prepareMeasureData = function(measure)
 {
   #Convert data to cm
-  raw_measure = read.csv('C:/Users/Nathan/Desktop/Stat419/measure-students.txt', header=TRUE, sep='|')
-  measure = unique(raw_measure)
-  measure[measure$person_id == '81a9f3915f64dc6edd329b89bea87d5e', 'units'] = 'cm'
+  raw_measure.df = read.csv(measure, header=TRUE, sep='|')
+  measure.df = unique(raw_measure.df)
+  measure.df[measure.df$person_id == '81a9f3915f64dc6edd329b89bea87d5e', 'units'] = 'cm'
   inchNames = c('in', 'In', 'inches', 'Inch')
-  rowsToConvert = measure$units %in% inchNames
-  columnsToConvert = colnames(measure[,4:26])
-  measure[rowsToConvert,columnsToConvert] = conv_unit(measure[rowsToConvert,columnsToConvert],'inch','cm')
-  measure$units = 'cm'
+  rowsToConvert = measure.df$units %in% inchNames
+  columnsToConvert = colnames(measure.df[,4:26])
+  measure.df[rowsToConvert,columnsToConvert] = conv_unit(measure.df[rowsToConvert,columnsToConvert],'inch','cm')
+  measure.df$units = 'cm'
   
   #Clean data for the gender, writing, and swinging variables
-  measure[measure$gender %in% c('M','male','Male','m'),'gender'] = 'm'
-  measure[measure$gender %in% c('F','female','Female','f'),'gender'] = 'f'
-  measure[!(measure$gender %in% c('m','f')),'gender'] = 'nb'
-  measure$gender = as.factor(measure$gender)
-  measure$writing = as.factor(substr(tolower(measure$writing),1,1))
-  measure$swinging = as.factor(substr(tolower(measure$swinging),1,1))
+  measure.df[measure.df$gender %in% c('M','male','Male','m'),'gender'] = 'm'
+  measure.df[measure.df$gender %in% c('F','female','Female','f'),'gender'] = 'f'
+  measure.df[!(measure.df$gender %in% c('m','f')),'gender'] = 'nb'
+  measure.df$gender = as.factor(measure.df$gender)
+  measure.df$writing = as.factor(substr(tolower(measure.df$writing),1,1))
+  measure.df$swinging = as.factor(substr(tolower(measure.df$swinging),1,1))
   
   #Clean data for the eye variable
-  errorEntry = na.omit(measure$eye == 'brown')
-  measure[errorEntry,'eye_color'] = 'brown'
-  measure[errorEntry,'eye'] = 'left'
-  measure$eye = substr(tolower(measure$eye),1,1)
-  measure[measure$eye %in% c('b','e'),'eye'] = 'b'
-  measure$eye = as.factor(measure$eye)
+  errorEntry = na.omit(measure.df$eye == 'brown')
+  measure.df[errorEntry,'eye_color'] = 'brown'
+  measure.df[errorEntry,'eye'] = 'left'
+  measure.df$eye = substr(tolower(measure.df$eye),1,1)
+  measure.df[measure.df$eye %in% c('b','e'),'eye'] = 'b'
+  measure.df$eye = as.factor(measure.df$eye)
   
   #Clean data for the eye color variable
-  measure[measure$eye_color == 'blue/green','eye_color'] = 'blue-green'
-  measure$eye_color = as.factor(tolower(measure$eye_color))
+  measure.df[measure.df$eye_color == 'blue/green','eye_color'] = 'blue-green'
+  measure.df$eye_color = as.factor(tolower(measure.df$eye_color))
   
   #Clean data for the ethnicity variable
-  measure$ethnicity = tolower(measure$ethnicity)
-  measure[measure$ethnicity == 'asain','ethnicity'] = 'asian'
-  measure[measure$ethnicity == 'caucasain','ethnicity'] = 'caucasian'
+  measure.df$ethnicity = tolower(measure.df$ethnicity)
+  measure.df[measure.df$ethnicity == 'asain','ethnicity'] = 'asian'
+  measure.df[measure.df$ethnicity == 'caucasain','ethnicity'] = 'caucasian'
   
-  measure[measure$ethnicity %in% c('anglo','caucasian','white italian', 'white non-hispanic'),'ethnicity'] = 'white'
-  measure[measure$ethnicity == 'african american','ethnicity'] = 'black'
-  measure[measure$ethnicity %in% c('latin american', 'latino'),'ethnicity'] = 'hispanic'
-  measure[measure$ethnicity %in% c('chinese','indian','japanese','korean','laotian','filipino'),'ethnicity'] = 'asian'
-  measure[!(measure$ethnicity %in% c('asian','black','hispanic','native american','pacific islander','white')),'ethnicity'] = 'mixed'
-  measure$ethnicity = as.factor(measure$ethnicity)
+  measure.df[measure.df$ethnicity %in% c('anglo','caucasian','white italian', 'white non-hispanic'),'ethnicity'] = 'white'
+  measure.df[measure.df$ethnicity == 'african american','ethnicity'] = 'black'
+  measure.df[measure.df$ethnicity %in% c('latin american', 'latino'),'ethnicity'] = 'hispanic'
+  measure.df[measure.df$ethnicity %in% c('chinese','indian','japanese','korean','laotian','filipino'),'ethnicity'] = 'asian'
+  measure.df[!(measure.df$ethnicity %in% c('asian','black','hispanic','native american','pacific islander','white')),'ethnicity'] = 'mixed'
+  measure.df$ethnicity = as.factor(measure.df$ethnicity)
   
-  measure;
+  n.rows = dim(measure.df)[1];
+  
+  getOne = c("hand.length", "hand.width", "hand.elbow", "elbow.armpit", "arm.reach", "foot.length", "floor.kneepit", "floor.hip", "floor.armpit");
+  for(i in 1:n.rows) {
+    for(col in getOne) {
+      rightSide = paste0(col,'.right')
+      leftSide = paste0(col,'.left')
+      measure.df[i,col] = mean(unlist(measure.df[i,c(rightSide,leftSide)]),na.rm=TRUE)
+    }
+  }
+  
+  measure.df[,c('side','units','hand.length.left','hand.length.right','hand.width.left','hand.width.right','hand.elbow.left','hand.elbow.right','elbow.armpit.left','elbow.armpit.right','arm.reach.left','arm.reach.right','foot.length.left','foot.length.right','floor.kneepit.left','floor.kneepit.right','floor.hip.left','floor.hip.right','floor.armpit.left','floor.armpit.right')] = NULL
+  measure.df = measure.df[,c(1:7,18:26,8:17)]
+  measure.df;
 }
